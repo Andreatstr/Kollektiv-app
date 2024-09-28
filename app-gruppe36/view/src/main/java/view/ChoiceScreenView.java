c4083a9998d9d99e94d0b781ee4972310558dce4
Kristian Håøy Rokne <krishro@stud.ntnu.no>
2024-09-28 17:18:08 +0200

    Resolve "Lage innlogging og valg scene + controllerne/view til dem"

---
+++ app-gruppe36/view/src/main/java/view/ChoiceScreenView.java
@@ -0,0 +1,33 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ChoiceScreenView {
+
+    @FXML
+    private Button BackFromChoiceScreen;
+
+    @FXML
+    private Button openShoppingListOverviewButton;
+
+    @FXML
+    private Button openWashingPlanOverviewButton;
+
+    @FXML
+    void ButtonBackFromChoiceScreen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ButtonOpenShoppingListOverview(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ButtonOpenWashingPlanOverview(ActionEvent event) {
+
+    }
+
+}
---
+++ app-gruppe36/view/src/main/java/view/LogInHouseIDView.java
@@ -0,0 +1,29 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LogInHouseIDView {
+
+    @FXML
+    private Button BackFromLogInHouseID;
+
+    @FXML
+    private Button loginHouseIDButton;
+
+    @FXML
+    private TextField loginHouseIDField;    // må legge til sjekk at ID finnes
+
+    @FXML
+    void ButtonBackFromLogInHouseID(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ButtonLoginHouseID(ActionEvent event) {
+
+    }
+
+}
