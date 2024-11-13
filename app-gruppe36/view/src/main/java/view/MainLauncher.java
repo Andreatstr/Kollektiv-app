5684555ca8d673a0e07f4e49af777d2af47d2bcd
Andrea Stromstad <andrea.stromstad@outlook.com>
2024-11-13 02:58:46 +0100

    fix: forsøk på jpackage

---
+++ app-gruppe36/view/src/main/java/view/MainLauncher.java
@@ -0,0 +1,8 @@
+package view;
+
+public class MainLauncher {
+    public static void main(String[] args) {
+        System.out.println("Application starting...");
+        App.launch(App.class, args);
+    }
+}
